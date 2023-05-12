53f6c61b4e8272d2e8aeaf5e49fea9860f81daae
oomeanujoo <anuzz8602@gmail.com>
2023-05-12 11:14:01 +0530

    with node modules

---
+++ src/app/navbar/navbar.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavbarRoutingModule } from './navbar-routing.module';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+
+@NgModule({
+  declarations: [
+    ToolbarComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    CommonModule,
+    NavbarRoutingModule
+  ]
+})
+export class NavbarModule { }
