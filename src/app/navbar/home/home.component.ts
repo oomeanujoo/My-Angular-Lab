f5417164d966c4259d6a6b267a01412f50123f2d
oomeanujoo <anuzz8602@gmail.com>
2023-05-23 13:31:29 +0530

    anuj

--- src/app/navbar/home/home.component.ts
+++ src/app/navbar/home/home.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HeroService } from 'src/app/hero.service';
+declare var $: any
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  allCity: any;
 
-  constructor() { }
+  constructor(private hs: HeroService) { }
 
   ngOnInit(): void {
+    debugger;
+    const that = this
+
+    $.cordys.authentication.sso
+      .authenticate('Arun', 'Arun')
+      .done((resp: any) => {
+        this.hs
+        .ajax(
+          'GetTbm_master1Objects',
+          'http://schemas.cordys.com/TBMMetadata',
+          { fromId: 1, toId: 100 }
+        )
+        .then((resp) => {
+          console.log(resp);
+          console.log($.cordys.json.findObjects(resp, 'tbm_master1'));
+          that.allCity = $.cordys.json.findObjects(resp, 'tbm_master1');
+        });
+        // this.heroService.stop();
+        // $('#exampleModal2').modal('hide');
+        // $('#exampleModal3').modal('show');
+        //  window.location.hash = '/dash';
+      })
+      .fail(function () {
+        that.hs.stop();
+        console.log('fail');
+      })
+      .always(function () {
+
+        console.log('always');
+      });
+
+
+
+
   }
 
 }
